7c6f5f8aeee3915559c850d4df72bd38edb1f3d9
aditya9219 <aditya.goyal9219@gmail.com>
2016-02-21 19:03:42 -0800

    Update serverSide.php

--- serverSide.php
+++ serverSide.php
@@ -19,17 +19,40 @@ switch ($method) {
        $statusCode =  '{"status":"ok"}';
        echo $statusCode;
     }
+    <?php
+error_reporting(0);
+$method = $_SERVER['REQUEST_METHOD'];
+$connIdWithTime = '{"12":"15","8":"10"}';
+
+$connIdWithTimeJson = json_decode($connIdWithTime);//creates the json Object with string
+//$connIdWithTimeJson = json_decode($connIdWithTime,true);//creates the json Array with string
+switch ($method) {
+  case 'PUT':
+    /*do_something_with_put($request);*/  
+    break;
+        
+  case 'GET':
+    if(isset($_GET['connId'])&& isset($_GET['timeout']))
+    {
+       $conId= $_GET['connId'];
+       $timeOut = $_GET['timeout'];
+       sleep($timeOut);
+       $statusCode =  '{"status":"ok"}';
+       echo $statusCode;
+    }
     if(isset($_GET['connId']))
     {
         $connId= $_GET['connId'];
         
-        //echo $connId;
-        $connIdStr = (string)$connId;
-        //echo $connIdStr;
-        //echo $connIdWithTimeJson[$connIdStr];
-        echo $connIdWithTimeJson->$connIdStr;
+        $remainingTime = $connIdWithTimeJson->$connId;
+        echo $remainingTime;
     }
     break;
+  
+  default:
+    break;
+}
+?>
   
   default:
     break;
